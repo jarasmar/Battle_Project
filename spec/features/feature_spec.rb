31ad9a848f7ead5c74504ff0518095a432805519
Gareth Harris <gdharris1000@gmail.com>
2020-03-04 14:57:06 +0000

    added form for player names

---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,11 @@
+# In spec/features, write a feature test that checks that the hompage says Testing infrastructure working!
+
+
+feature 'Name entry form' do
+  scenario 'player form' do
+    visit ('/')
+    fill_in ('player1'), with: 'Gareth'
+    fill_in ('player2'), with: 'Jara'
+    click_button 'Submit'
+  end
+end
